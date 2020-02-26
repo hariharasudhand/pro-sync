34531d83d0a721d973d029b0142ce517b7d777c6
dhinakaran-72 <dhinakar@weeroda.com>
2020-02-26 18:18:09 +0530

    org onboard added

---
+++ prosync.portal/users/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
+from .models import Organization, Profile
+
+class UserRegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class OrgRegisterForm(forms.ModelForm):
+
+    class Meta:
+        model = Organization
+        fields = ['org_name', 'address']
+
+class OrgUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Organization
+        fields = ['org_type']
+
+class UserUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['phone', 'photo']
--- prosync.portal/users/urls.py
+++ prosync.portal/users/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views as user_view
 
 
 urlpatterns = [
-    # path('register/', user_view.register, name='register'),
-    # path('profile/', user_view.profile, name='profile'),
+
+    path('register/', user_view.register, name='register'),
+    path('profile/', user_view.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('success/', auth_views.LoginView.as_view(template_name='app/main.html'), name='success'),
     path('logout/', auth_views.LogoutView.as_view(template_name='app/index.html'), name='logout'),
+
+    # organization links
+    path('org-onboard/', user_view.orgOnboard, name='org-onboard'),
 
 ]
