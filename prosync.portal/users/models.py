194763292062063404c307c8ee43a9051d6decdd
dhinakaran-72 <dhinakar@weeroda.com>
2020-03-02 15:35:03 +0530

    new features added

--- prosync.portal/users/models.py
+++ prosync.portal/users/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth.models import User, Group
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from multiselectfield import MultiSelectField
 
 MODULES = ( ('Admin', 'Admin'), ('Product', 'Product'), ('Billing', 'Billing'),
             ('Batch', 'Batch'), ('Support', 'Support'), ('Offer', 'Offer') )
@@ -14,13 +15,39 @@ class Organization(models.Model):
     org_name = models.CharField(max_length=50, null=False, blank=False, default='')
     address = models.CharField(max_length=250, null=True, blank=True)
     org_type = models.CharField(max_length=20, choices=ORG_TYPE, default='Retailer')
-    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     status = models.CharField(max_length=20, null=False, blank=False, default='INACTIVE')
     date_added = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.org_name
+
+class RolePermission(models.Model):
+    role_name = models.CharField(max_length=50, default='')
+    module = MultiSelectField(choices=MODULES, default='Support')
+    org = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    create = models.BooleanField(default=False)
+    view = models.BooleanField(default=False)
+    update = models.BooleanField(default=False)
+    delete = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, default='ACTIVE')
+    date_added = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.role_name
+
+class Groups(models.Model):
+    group_name = models.CharField(max_length=25, default='')
+    org = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
+    role_permission = models.ForeignKey(RolePermission, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(max_length=25, default='ACTIVE')
+    date_added = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.group_name
+
 
 class Profile(models.Model):
     email = models.CharField(max_length=50, null=False, blank=False, default='')
@@ -29,13 +56,13 @@ class Profile(models.Model):
     org = models.ForeignKey(Organization, on_delete=models.SET_NULL, null=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     user_token = models.CharField(max_length=80, null=True, blank=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
     status = models.CharField(max_length=20, null=False, blank=False, default='INACTIVE')
     date_added = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.email
-
+        return self.user
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
