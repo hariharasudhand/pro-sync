75787d3c7f7060ffe17a5501179ee371db669347
hariweeroda <hari@weeroda.com>
2020-03-10 10:27:39 +0530

    api docs manual
    api docs

--- prosync.portal/gentelella/settings.py
+++ prosync.portal/gentelella/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'gentelella.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pro-sync',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'pro-sync',
+    #    'USER': 'postgres',
+    #    'PASSWORD': '',
+    #    'HOST': 'localhost',
+    #    'PORT': '',
+    #}
 }
 
 
